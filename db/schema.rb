ff2adf5dc3f18f70b40fd67836ca5983f88c3894
Riz <mahmoud.eleresousy@gmail.com>
2023-10-26 14:11:41 +0300

    Add User migration and run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_26_044910) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_110545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
     t.text "greetings"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
