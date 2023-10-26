01811161d81d85d5df880db0f33e416ba24c5064
kessie <kayp3477@gmail.com>
2023-10-26 10:58:12 +0000

    Add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
+  # GET /users/:id
+  def new; end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:get, :create)
+  end
+end
