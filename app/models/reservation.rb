d31f7bd78ce55bc5ebede9b16179e4bcb7f0bb02
kessie <kayp3477@gmail.com>
2023-10-27 15:03:33 +0000

    Add validations to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :unit
+
+  validates :user, presence: true
+  validates :unit, presence: true
+  validates :date, presence: true
 end
