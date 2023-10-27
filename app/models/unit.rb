19cbf666962d47870e4ff28a49e1a28e9c7ca523
Riz <mahmoud.eleresousy@gmail.com>
2023-10-27 16:00:21 +0200

    Update Unit Model validations, attributes, and Associations

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,20 @@
 class Unit < ApplicationRecord
+  #Associations
   belongs_to :user
+
+  #Attributes
+  attribute name: :text
+  attribute description: :text
+  attribute image: :text, array: true, default: []
+  attribute price: :integer
+  attribute type: :text
+  attribute location: :text
+
+  #Validations
+  validate :name, presence: true, length: { maximum: 255 }
+  validate :description, presence: true, length: { maximum: 255 }
+  validate :image
+  validate :price, presence: true, numiricality: { greater_than: 0 }
+  validate :type, presence: true, length: { maximum: 255 }
+  validate :location, presence: true, length: { maximum: 255 }
 end
