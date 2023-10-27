38731562c54708e3bc3ff4ea2b10e4bf652c6822
Riz <mahmoud.eleresousy@gmail.com>
2023-10-27 16:00:45 +0200

    Update and run migration

--- db/migrate/20231027134910_create_units.rb
+++ db/migrate/20231027134910_create_units.rb
@@ -1,12 +1,12 @@
 class CreateUnits < ActiveRecord::Migration[7.1]
   def change
     create_table :units do |t|
-      t.text :name
-      t.text :description
-      t.text :image
-      t.integer :price
-      t.text :type
-      t.text :location
+      t.text :name, null: false
+      t.text :description, default: "", null: false
+      t.text :image, array: true, default: []
+      t.integer :price, null: false
+      t.text :type, null: false
+      t.text :location, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_26_110545) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_27_134910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,10 +20,24 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_26_110545) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "units", force: :cascade do |t|
+    t.text "name", null: false
+    t.text "description", default: "", null: false
+    t.text "image", default: [], array: true
+    t.integer "price", null: false
+    t.text "type", null: false
+    t.text "location", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_units_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.text "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "units", "users"
 end
