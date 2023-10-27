e5c6836742e588d721f6a816babf529ed0b30fa0
SAZZAD1Q2 <kurulia115@gmail.com>
2023-10-27 18:56:29 +0600

    new_user add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
-  # Assosiations
+
+  # user name to save
+  # if the user exist then save it to the local storage. if not we create new user and sav e to the local storage.
 
   # Attributes
   attribute :name, :text
 
   # Validations
   validate :name, presence: true, length: { maximum: 255 }
+#user.find_or_create_by(name:..user name)def
+def findCreateUser (user_name)
+@new_user = User.find_or_create_by(name:user_name)
+
+
+end
 end
