5256068efcf0b2d0e0fffedeaa4a8ff038ec355a
kessie <kayp3477@gmail.com>
2023-10-28 13:45:12 +0000

    Created validation instances

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    it { should validate_presence_of(:name) }
+    it 'is not valid without a name' do
+      user = User.new(name: nil)
+      expect(user).not_to be_valid
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    it 'is valid with valid attributes' do
+      user = User.new(name: 'Prosper Kessie')
+      expect(user).to be_valid
+    end
+
     it { should validate_length_of(:name).is_at_most(255) }
   end
 
