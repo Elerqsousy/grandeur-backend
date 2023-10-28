dc042d3bab8e5df45d412bd14c09187bdafbcff4
Riz <mahmoud.eleresousy@gmail.com>
2023-10-28 14:07:18 +0200

    Add new migration to fix column name reserved by system issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_27_143929) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_28_120344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_27_143929) do
     t.text "description", default: "", null: false
     t.text "image", default: [], array: true
     t.integer "price", null: false
-    t.text "type", null: false
+    t.text "unit_type", null: false
     t.text "location", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
