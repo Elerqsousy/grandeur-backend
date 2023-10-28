409ebd395cee24907ef7aa90bedfe2791c2ff5b1
SAZZAD1Q2 <kurulia115@gmail.com>
2023-10-28 21:06:12 +0600

    disable attribute in unit model for checking

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,14 +3,7 @@ class Unit < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy, foreign_key: :unit_id
 
-  # Attributes
-  attribute :name, :text
-  attribute :description, :text
-  attribute :image, :text, array: true, default: []
-  attribute :price, :integer
-  attribute :unit_type, :text
-  attribute :location, :text
-  attribute :user_id, :integer
+  
 
   # Validations (use 'validates' instead of 'validate')
   validates :name, presence: true, length: { maximum: 255 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,9 @@ class User < ApplicationRecord
   has_many :units, dependent: :destroy, foreign_key: :user_id
   has_many :reservations, dependent: :destroy, foreign_key: :user_id
 
-  # Attributes
-  attribute :name, :text
 
   # Validations
-  validate :name, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
 
   def find_create_user(user_name)
     new_user = User.find_or_create_by!(name: user_name)
