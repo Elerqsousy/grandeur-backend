21fa2e9729b7b0a445eaac2572ad5f94b2cff264
kessie <kayp3477@gmail.com>
2023-10-28 14:02:28 +0000

    Add instances to interact with model

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -7,8 +7,30 @@ RSpec.describe Reservation, type: :model do
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:user) }
-    it { should validate_presence_of(:unit) }
-    it { should validate_presence_of(:date) }
+    it 'is not valid without a user' do
+      reservation = Reservation.new(unit: Unit.new, date: Date.today)
+      expect(reservation).not_to be_valid
+      expect(reservation.errors[:user]).to include("can't be blank")
+    end
+
+    it 'is not valid without a unit' do
+      reservation = Reservation.new(user: User.new, date: Date.today)
+      expect(reservation).not_to be_valid
+      expect(reservation.errors[:unit]).to include("can't be blank")
+    end
+
+    it 'is not valid without a date' do
+      reservation = Reservation.new(user: User.new, unit: Unit.new)
+      expect(reservation).not_to be_valid
+      expect(reservation.errors[:date]).to include("can't be blank")
+    end
+
+    it 'is valid with valid attributes' do
+      user = User.create(name: 'Prosper Kessie')
+      unit = Unit.create(name: 'Keelson court', description: '2,3 and 4 bedroom townhouses', price: 1000,
+                         location: 'Accra')
+      reservation = Reservation.new(user:, unit:, date: Date.today)
+      expect(reservation).to be_valid
+    end
   end
 end
