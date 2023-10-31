4a6c3355d994e3026311b342bc00df5104495dd8
kessie <kayp3477@gmail.com>
2023-10-31 13:05:26 +0000

    Add routes for updated actions in user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[ new create ]
   resources :units, only: %i[ index show new create destroy ]
   resources :reservations, only: %i[ index show new create destroy ]
-
+  resources :users, only: [:index, :show, :create, :login] do
+    collection do
+      post 'login', to: 'users#login'
+    end
+  end
   root "units#index"
 end
