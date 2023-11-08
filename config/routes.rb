2a914b3018cf6f68a13efa34f6c10a613a70c8c8
kessie <kayp3477@gmail.com>
2023-11-08 08:34:35 +0000

    Add update and destroy actions to users route for testing purposes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :units, only: %i[ index show new create destroy ]
   resources :reservations, only: %i[ index show new create destroy ]
-  resources :users, only: [:index, :show, :create, :login] do
+  resources :users, only: [:index, :show, :create, :update, :destroy, :login] do
     collection do
       post 'login', to: 'users#login'
     end
