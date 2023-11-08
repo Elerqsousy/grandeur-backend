4d03cfa7525baf21a0747706a529a9b666be8e80
kessie <kayp3477@gmail.com>
2023-11-08 08:17:33 +0000

    Add RSpec test to ensure it returns a JSON response with reservation details

--- spec/controllers/reservations_controller_spec.rb
+++ spec/controllers/reservations_controller_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe ReservationsController, type: :controller do
       expect(response_data['reservations'].length).to eq(2)
     end
   end
+
+  describe 'GET #show' do
+    it 'returns a JSON response with the reservation details' do
+      reservation = create(:reservation, user:)
+      get :show, params: { id: reservation.id }
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to include('application/json')
+      expect(assigns(:reservation)).to eq(reservation)
+    end
+  end
 end
