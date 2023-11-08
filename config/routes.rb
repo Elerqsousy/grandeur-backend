d276c02014038d736c724a16bf1ee7ff6d03fd54
kessie <kayp3477@gmail.com>
2023-11-08 08:36:00 +0000

    Add update action to units and reservation route for testing pursoses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :units, only: %i[ index show new create destroy ]
-  resources :reservations, only: %i[ index show new create destroy ]
+  resources :units, only: %i[ index show new create update destroy ]
+  resources :reservations, only: %i[ index show new update create destroy ]
   resources :users, only: [:index, :show, :create, :update, :destroy, :login] do
     collection do
       post 'login', to: 'users#login'
