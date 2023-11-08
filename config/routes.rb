e6285f0ab11f020d0b0e27345571d4e2ba3ba6b0
kessie <kayp3477@gmail.com>
2023-11-08 16:11:46 +0000

    Added Rswag API and UI documentation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :units, only: %i[ index show new create destroy ]
   resources :reservations, only: %i[ index show new create destroy ]
   resources :users, only: [:index, :show, :create, :login] do
