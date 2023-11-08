6f7af54245ac20784f9d7b9cae2ef1a27beda07f
kessie <kayp3477@gmail.com>
2023-11-08 08:08:35 +0000

    Add tests to handle user presence and absence with appropriate response statuses

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  describe 'POST #login' do
+    it 'returns a successful response when user is found' do
+      user = User.create(name: 'Prosper Kessie')
+      post :login, params: { name: user.name }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a not found response when user is not found' do
+      post :login, params: { name: 'Sazzad Hossain' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
 end
