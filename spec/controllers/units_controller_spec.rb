adaf8c0d81f149c9e845815b1b23b2baa364ed0a
kessie <kayp3477@gmail.com>
2023-11-08 08:25:43 +0000

    Added RSpec test for show action, verifying JSON response with unit details and response status.

--- spec/controllers/units_controller_spec.rb
+++ spec/controllers/units_controller_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe UnitsController, type: :controller do
       expect(response.content_type).to include('application/json')
     end
   end
+
+  describe 'GET #show' do
+    it 'returns a JSON response with the unit details' do
+      user = create(:user)
+      unit = create(:unit, user:)
+      get :show, params: { id: unit.id }
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to include('application/json')
+    end
+  end
 end
