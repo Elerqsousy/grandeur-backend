e2e939a587417eb80335b9843db2a868d2867785
kessie <kayp3477@gmail.com>
2023-11-08 07:30:00 +0000

    Required factory-bot-rails gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'factory_bot_rails'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -6,6 +8,8 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
+  config.include FactoryBot::Syntax::Methods
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
